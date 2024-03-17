4ad7963bf23faa2c8bb994f3c3c079349a4ac31d
B Hemanth Goud <123377431+hemanth-user13@users.noreply.github.com>
2024-03-17 12:50:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
 echo "Building the project.... "
 python3.9 -m pip install -r requirements.txt
+
+
+echo "make migrations..."
+python3.9 manage.py makemigrations  --noinput 
+
+echo "collect static......."
+python3.9 manage.py collectstatic --noinput --clear
