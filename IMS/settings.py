67094284fba95c8b6a23dfa152bf79753a976334
B Hemanth Goud <123377431+hemanth-user13@users.noreply.github.com>
2024-03-17 13:06:43 +0530

    Update settings.py

--- IMS/settings.py
+++ IMS/settings.py
@@ -79,22 +79,22 @@ WSGI_APPLICATION = 'IMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',  # PostgreSQL database backend
-        'NAME': 'test',  # Database name
-        'USER': 'postgres',  # Database user
-        'PASSWORD': 'Bachi@123',  # Database password
-        'HOST': 'localhost',  # Database host (assuming it's running locally)
-        'PORT': '5432',  # Database port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',  # PostgreSQL database backend
+#         'NAME': 'test',  # Database name
+#         'USER': 'postgres',  # Database user
+#         'PASSWORD': 'Bachi@123',  # Database password
+#         'HOST': 'localhost',  # Database host (assuming it's running locally)
+#         'PORT': '5432',  # Database port
+#     }
+# }
 
 
 
